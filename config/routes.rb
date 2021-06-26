fda8cbee2d785628771ffb09d4965e3a2a91dd21
ryo maruyama <t.t.uncle0803@gmail.com>
2021-06-26 21:28:49 +0900

    初回

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
+
+  resources :users,only: [:new,:create,:show]
+  resources :sessions, only: [:new, :create, :destroy]
+  root :to => 'users#new'
+end
