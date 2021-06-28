fd4d31b3840093348895e66a357f1a4d7e191d45
ryo maruyama <t.t.uncle0803@gmail.com>
2021-06-28 12:08:58 +0900

    新規投稿の際空欄禁止機能追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,4 +3,7 @@ class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  validates :content, presence: true
+  validates :title, presence: true
+  validates :image, presence: true
 end
